62e10afc5f6dffb515de50b5905c914268e38c05
Sirigiri Rakesh <94678816+Rakesh-sirigiri@users.noreply.github.com>
2024-10-12 21:44:45 +0530

    Add files via upload

---
+++ Grade Calculator in Python.py
@@ -0,0 +1,35 @@
+print("Enter Marks obtained in 5 subjectts: ")
+total1 = 44
+total2 = 57
+total3 = 67
+total4 = 87
+total5 = 58
+tot = total1 + total2 + total3 + total4 + total5
+avg = tot / 5
+if avg >= 91 and avg <=100:
+    print("your Grade is A1")
+elif avg >= 81 and avg < 91:
+    print("your Grade is A2")
+elif avg >= 71 and avg < 81:
+    print("your Grade is B1")
+elif avg >= 61 and avg < 71:
+    print("your Grade is B2")
+elif avg >= 51 and avg < 61:
+    print("your Grade is C1")
+elif avg >= 41 and avg < 51:
+    print("your Grade is C2")
+elif avg >= 33 and avg < 41:
+    print("your Grade is D")
+elif avg >= 21 and avg < 33:
+    print("your Grade is E1")
+elif avg >= 0 and avg < 21:
+    print("your Grade is E2")
+else:
+    print("invalid input!")
+        
+        
+        
+        
+        
+        
+        
